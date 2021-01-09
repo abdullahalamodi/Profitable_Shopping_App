f3f07dfb4a26a7f1b168b4c9cf441c27cae420d6
Maimoona Alrozmi <memo38605@gmail.com>
2021-01-09 19:53:39 +0300

    Ui design of categories

--- app/src/main/java/com/finalproject/profitableshopping/view/MainActivity.kt
+++ app/src/main/java/com/finalproject/profitableshopping/view/MainActivity.kt
@@ -3,10 +3,43 @@ package com.finalproject.profitableshopping.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.finalproject.profitableshopping.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    setContent("Home")
+                    true
+                }
+                R.id.menu_shopping_cart -> {
+                    setContent("Cart")
+                    true
+                }
+                R.id.menu_search -> {
+                    setContent("Search")
+                    true
+                }
+                R.id.menu_notification -> {
+                    setContent("Profile")
+                    true
+                }
+                R.id.menu_profile -> {
+                    setContent("Profile")
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun setContent(content: String) {
+        setTitle(content)
+        // tvLabel.text = content
+
     }
 }
