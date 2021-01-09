57544e41a662bd7453b7ff0b82cd78125c86e170
haytham taha alsharany <talsharany@gmail.com>
2021-01-09 17:42:34 +0300

    haytham first commit to mange category

---
+++ app/src/main/java/com/finalproject/profitableshopping/data/ShoppingRepositry.kt
@@ -0,0 +1,43 @@
+package com.finalproject.profitableshopping.data
+
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.finalproject.profitableshopping.data.api.ShoppingApi
+import com.finalproject.profitableshopping.data.models.CategoryModel
+
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ShoppingRepositry {
+     var shoppingApi: ShoppingApi
+
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("")
+            .build()
+        shoppingApi = retrofit.create(ShoppingApi::class.java)
+    }
+    fun createCategory(category: HashMap<Any,Any>): Call<String> {
+      return shoppingApi.createCategory(category)
+    }
+    fun getCategory(id:Int):Call<CategoryModel>{
+        return  shoppingApi.fetchCaregory(id)
+    }
+    fun getAllCategories():Call<List<CategoryModel>>{
+        return shoppingApi.fetchAllCaregories()
+
+    }
+
+    fun updateCategory(catId:Int,category: HashMap<Any, Any>):Call<String>{
+        return  shoppingApi.editCategory(catId,category)
+    }
+    fun deleteCategory(catId:Int):Call<String>{
+        return shoppingApi.deleteCategory(catId)
+    }
+}
--- app/src/main/java/com/finalproject/profitableshopping/view/MainActivity.kt
+++ app/src/main/java/com/finalproject/profitableshopping/view/MainActivity.kt
@@ -3,10 +3,18 @@ package com.finalproject.profitableshopping.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.finalproject.profitableshopping.R
+import com.finalproject.profitableshopping.categorty.CategoryCrudFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val isFragmentContainerEmpty = savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            val fragmentManager=supportFragmentManager
+            val fragmentTransaction=fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, CategoryCrudFragment.newInstance())
+                .commit()
+        }
     }
 }
