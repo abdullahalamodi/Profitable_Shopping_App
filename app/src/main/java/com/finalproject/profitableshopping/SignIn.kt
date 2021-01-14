7a22c3cc20030e9ac7fc34f0b27a9520e6fd1f40
abdullahalamodi <abdullahalamodi@bootfi.com>
2021-01-14 10:24:35 +0300

    join sign up and sign in and and check user if login

--- app/src/main/java/com/finalproject/profitableshopping/SignIn.kt
+++ app/src/main/java/com/finalproject/profitableshopping/SignIn.kt
@@ -1,7 +1,9 @@
 package com.finalproject.profitableshopping
 
 import android.app.ProgressDialog
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
@@ -18,28 +20,29 @@ import kotlinx.android.synthetic.main.activity_sign_in.*
 class SignIn : AppCompatActivity() {
 
     var mAuth: FirebaseAuth? = null
-   /* lateinit var email_login : EditText
-    lateinit var passWord_login : EditText
-    lateinit var btn_login : Button
-    lateinit var sign_up : TextView*/
+    /* lateinit var email_login : EditText
+     lateinit var passWord_login : EditText
+     lateinit var sign_up : TextView*/
 
+     lateinit var signUpBtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-
+        signUpBtn = findViewById(R.id.sign_up)
         btn_login.setOnClickListener {
             var email = email_login.text.toString()
             var password = passWord_login.text.toString()
 
             if (email.isNotEmpty() && password.isNotEmpty()) {
 
-                if(!isValidEmail(email)){
+                if (!isValidEmail(email)) {
                     Toast.makeText(this, "Please enter valid email", Toast.LENGTH_SHORT).show()
                     return@setOnClickListener
                 }
 
-                if(password.length<6){
-                    Toast.makeText(this, "Password must be 6 digit at least ", Toast.LENGTH_SHORT).show()
+                if (password.length < 6) {
+                    Toast.makeText(this, "Password must be 6 digit at least ", Toast.LENGTH_SHORT)
+                        .show()
                     return@setOnClickListener
                 }
 
@@ -60,7 +63,11 @@ class SignIn : AppCompatActivity() {
     }
 
     private fun login(email: String, password: String) {
-        var p= ProgressDialog(this)
+        if (CheckAdmin(email, password)) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        val p = ProgressDialog(this)
         p.setMessage("please wait")
         p.setCanceledOnTouchOutside(false)
         p.show()
@@ -70,15 +77,23 @@ class SignIn : AppCompatActivity() {
                 p.dismiss()
                 if (it.isSuccessful) {
                     verifyEmailAddress()
+                    saveUserToken(it.result?.user?.uid.toString())
+                    saveUserData(email,password)
                 } else {
-                    Toast.makeText(this, "Your login Failed ${it.exception.toString()}", Toast.LENGTH_LONG)
+                    Toast.makeText(
+                        this,
+                        "Your login Failed ${it.exception.toString()}",
+                        Toast.LENGTH_LONG
+                    )
                         .show()
                 }
             }
 
-        sign_up.setOnClickListener {
-            var intent= Intent(this,SignIn::class.java)
+        signUpBtn.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
+            Toast.makeText(this, " sign up", Toast.LENGTH_LONG)
+                .show()
         }
 
     }
@@ -87,14 +102,66 @@ class SignIn : AppCompatActivity() {
     private fun verifyEmailAddress() {
 
         val user: FirebaseUser? = mAuth?.currentUser
-        if(user!!.isEmailVerified){
+        if (user!!.isEmailVerified) {
+
             Toast.makeText(this, "Done", Toast.LENGTH_LONG).show()
             var intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
-        }else{
+        } else {
             Toast.makeText(this, "Please verify your account", Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun saveUserToken(token: String) {
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences(
+            sharedPrefFile,
+            Context.MODE_PRIVATE
+        )
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString(tokenKey, token)
+        editor.apply()
+        editor.commit()
+    }
+
+    private fun saveUserData(email: String, password: String) {
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences(
+            sharedPrefFile,
+            Context.MODE_PRIVATE
+        )
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString(emailKey, email)
+        editor.putString(passwordKey, password)
+        editor.apply()
+        editor.commit()
+    }
+
+
+
+    private fun CheckAdmin(email: String, password: String): Boolean {
+        val admin = adminAccount()
+        if (
+            admin["email"] == email &&
+            admin["password"] == password
+        ) {
+            saveUserToken("admin")
+            return true
+        }
+        return false
+    }
+
+    private fun adminAccount(): HashMap<String, String> {
+        val admin = HashMap<String, String>()
+        admin["email"] = "admin@admin.com"
+        admin["password"] = "12345"
+        return admin
+    }
+
+    companion object {
+        private const val sharedPrefFile = "user_pref";
+        private const val tokenKey = "token_key";
+        private const val emailKey = "email_key";
+        private const val passwordKey = "password_key";
 
     }
-    }
+}
--- app/src/main/java/com/finalproject/profitableshopping/view/MainActivity.kt
+++ app/src/main/java/com/finalproject/profitableshopping/view/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.finalproject.profitableshopping.view
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.finalproject.profitableshopping.R
+import com.finalproject.profitableshopping.SignIn
 import kotlinx.android.synthetic.main.activity_main.*
 import com.finalproject.profitableshopping.view.category.CategoryFragment
 import com.finalproject.profitableshopping.view.products.fragments.AddProductFragment
 import com.finalproject.profitableshopping.view.products.fragments.ProductDetailsFragment
 import com.finalproject.profitableshopping.view.products.fragments.ProductListFragment
 
-class MainActivity : AppCompatActivity(), ProductListFragment.Callbacks,AddProductFragment.Callbacks {
+class MainActivity : AppCompatActivity(), ProductListFragment.Callbacks,
+    AddProductFragment.Callbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //hide app bar elevation
         supportActionBar?.elevation = 0.0f
+
 
         val isFragmentContainerEmpty = savedInstanceState == null
         if (isFragmentContainerEmpty) {
@@ -87,15 +93,38 @@ class MainActivity : AppCompatActivity(), ProductListFragment.Callbacks,AddProdu
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_add_product -> {
-                setCurrentFragment(AddProductFragment.newInstance())
+                if (getUserToken() != null)
+                    setCurrentFragment(AddProductFragment.newInstance())
+                else {
+                    val intent = Intent(this, SignIn::class.java).apply {
+//                        putExtra(EXTRA_MESSAGE, message)
+                    }
+                    startActivity(intent)
+                }
                 true
             }
             R.id.menu_my_products -> {
-                setCurrentFragment(ProductListFragment.newInstance())
+                if (getUserToken() != null)
+                    setCurrentFragment(ProductListFragment.newInstance())
+                else {
+                    val intent = Intent(this, SignIn::class.java).apply {
+//                        putExtra(EXTRA_MESSAGE, message)
+                    }
+                    startActivity(intent)
+                }
                 true
             }
             R.id.menu_categories -> {
-                setCurrentFragment(CategoryFragment.newInstance())
+                if (getUserToken() != null && getUserToken() == "Admin")
+                    setCurrentFragment(CategoryFragment.newInstance())
+                else if (getUserToken() != "Admin") {
+                    Toast.makeText(this, "yor are not admin", Toast.LENGTH_SHORT).show()
+                } else {
+                    val intent = Intent(this, SignIn::class.java).apply {
+//                        putExtra(EXTRA_MESSAGE, message)
+                    }
+                    startActivity(intent)
+                }
                 true
             }
             else -> return super.onOptionsItemSelected(item)
@@ -104,6 +133,21 @@ class MainActivity : AppCompatActivity(), ProductListFragment.Callbacks,AddProdu
 
     override fun onSuccessAddProduct() {
         setCurrentFragment(ProductListFragment.newInstance())
+    }
+
+    private fun getUserToken(): String? {
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences(
+            sharedPrefFile,
+            Context.MODE_PRIVATE
+        )
+        return sharedPreferences.getString(tokenKey, null)
+    }
+
+
+    companion object {
+        private const val sharedPrefFile = "user_pref";
+        private const val tokenKey = "token_key";
+
     }
 
 
