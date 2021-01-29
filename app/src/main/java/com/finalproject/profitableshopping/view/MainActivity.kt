c63caa9e943d2a95245298507c398d143b7a3208
Maimoona Alrozmi <memo38605@gmail.com>
2021-01-29 17:23:09 +0300

    Init some update in contact us fragment

--- app/src/main/java/com/finalproject/profitableshopping/view/MainActivity.kt
+++ app/src/main/java/com/finalproject/profitableshopping/view/MainActivity.kt
@@ -93,6 +93,7 @@ class MainActivity : AppCompatActivity(),
                     setCurrentFragment(CartFragment.newInstance())
                     true
                 }
+
                /* R.id.menu_search -> {
 //                    setContent("Search")
                     setCurrentFragment(ShowAllProductsFragment.newInstance())
